d2a62ce911e2bb77d2119c48b56e8fb47dbc6400
ajafik <oluwafikayoajayi@gmail.com>
2020-05-03 19:04:13 +0100

    app comp cleaning up and tried PrimeNG comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CardModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
